032307a9207d5151347b6a4ea445ec7fde9c5b9f
s2019w29 <s2019w29@e-mirim.hs.kr>
2021-04-14 16:21:03 +0900

    polls/admin QuestionAdimin, models was_published_recently

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,10 @@
-from datetime import timezone
+
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -13,18 +14,15 @@ from django.shortcuts import get_object_or_404, render
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
-    class IndexView(generic.ListView):
-        template_name = 'polls/index.html'
-        context_object_name = 'latest_question_list'
-
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
